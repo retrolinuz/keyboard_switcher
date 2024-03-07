5fdcb8f0b7d06b0f2cf0f39ff852a96d2faf5eb2
Mert Hakki Bingol <3320399+retrolinuz@users.noreply.github.com>
2024-03-07 16:28:44 -0700

    Update switch_kb.sh

--- switch_kb.sh
+++ switch_kb.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-x="$(setxkbmap -query | awk 'NR == 3 {print $2 }')"
-if [ $x == "us" ]
-then
-	setxkbmap -layout tr
-	notify-send "Keyboard set to TR" "" -t 2
+
+# Query the current keyboard layout
+current_layout=$(setxkbmap -query | awk '/layout:/ {print $2}')
+
+# Toggle the keyboard layout and notify the user
+if [ "$current_layout" = "us" ]; then
+  setxkbmap -layout tr
+  notify-send "Keyboard set to TR" -t 2000
 else
-	setxkbmap -layout us
-	notify-send "Keyboard set to US" "" -t 2
+  setxkbmap -layout us
+  notify-send "Keyboard set to US" -t 2000
 fi
